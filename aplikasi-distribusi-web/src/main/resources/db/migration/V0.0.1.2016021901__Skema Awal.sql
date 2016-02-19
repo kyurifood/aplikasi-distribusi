3ad36b0c181454ff091b1869274c449f57644ac5
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-19 13:57:48 +0700

    starter project

---
+++ aplikasi-distribusi-web/src/main/resources/db/migration/V0.0.1.2016021901__Skema Awal.sql
@@ -0,0 +1,25 @@
+-- Tabel Kabupaten --
+create table kabupaten (
+    id varchar(255) primary key
+) Engine=InnoDB ;
+
+
+-- Tabel Kecamatan --
+
+-- Tabel Kelurahan --
+
+-- Tabel Kodepos --
+
+-- Tabel User --
+
+-- Tabel Role --
+
+-- Tabel Permission --
+
+-- Tabel Produk --
+
+-- Tabel Distributor --
+
+-- Tabel Marketer --
+
+-- Tabel Customer --
