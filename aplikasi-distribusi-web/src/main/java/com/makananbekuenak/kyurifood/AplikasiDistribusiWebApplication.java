1282ff455d0c24aa719b46956223b352889136ae
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-19 13:44:54 +0700

    starter aplikasi

---
+++ aplikasi-distribusi-web/src/main/java/com/makananbekuenak/kyurifood/AplikasiDistribusiWebApplication.java
@@ -0,0 +1,12 @@
+package com.makananbekuenak.kyurifood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiDistribusiWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiDistribusiWebApplication.class, args);
+	}
+}
