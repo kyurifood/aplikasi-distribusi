9c40b726f47e6834338b8ab48a9d1af1f1ce625b
Endy Muhardin <endy.muhardin@gmail.com>
2016-02-19 14:10:41 +0700

    implement entity kabupaten

---
+++ aplikasi-distribusi-web/src/main/java/com/makananbekuenak/kyurifood/dao/KabupatenDao.java
@@ -0,0 +1,8 @@
+package com.makananbekuenak.kyurifood.dao;
+
+import com.makananbekuenak.kyurifood.entity.Kabupaten;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface KabupatenDao extends PagingAndSortingRepository<Kabupaten, String> {
+
+}
