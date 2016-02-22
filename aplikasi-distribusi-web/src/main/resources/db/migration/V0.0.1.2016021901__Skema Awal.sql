707723d62d58ed4ad118a2b2537ec2e17fddd61f
gilangsuleman <gilang.mhs13@ti.poligon.ac.id>
2016-02-22 15:11:13 +0800

    update

--- aplikasi-distribusi-web/src/main/resources/db/migration/V0.0.1.2016021901__Skema Awal.sql
+++ aplikasi-distribusi-web/src/main/resources/db/migration/V0.0.1.2016021901__Skema Awal.sql
@@ -1,9 +1,9 @@
 -- Tabel Kabupaten --
 create table kabupaten (
-    id varchar(255) primary key unique,
-    kode varchar(255) NOT NULL unique,
-    nama varchar(255) NOT NULL
-) Engine=InnoDB;
+id varchar(255) primary key unique,
+kode varchar(255) NOT NULL unique,
+nama varchar(255) NOT NULL
+)Engine=InnoDB;
 
 -- Tabel Kecamatan --
 
@@ -13,10 +13,10 @@ create table kabupaten (
 
 -- Tabel User --
 create table user (
-username varchar(255) not null unique,
+username varchar(255)not null unique,
 email varchar(255) not null unique,
 fullname varchar(255) not null
-) Engine=InnoDB;
+)Engine=InnoDB;
 -- Tabel Role --
 
 -- Tabel Permission --
@@ -26,5 +26,13 @@ fullname varchar(255) not null
 -- Tabel Distributor --
 
 -- Tabel Marketer --
-
+create table marketer (
+kode varchar(255) not null unique,
+nama varchar(255) not null,
+hp varchar(255) not null,
+pinbb varchar(255),
+email varchar(255) not null,
+rekening varchar(255) not null
+alamat varchar(255) not null
+)Engine=InnoDB;
 -- Tabel Customer --
---
+++ aplikasi-distribusi-web/src/test/resources/mysql/sample-marketer.sql
@@ -0,0 +1,2 @@
+insert into marketer (kode, nama, hp, pinbb, email, rekening, alamat) values
+('123','market','085397477853','abcdef','0123456789','jln. Sapta Marga');
