ea28d318453371bd2b44ce6ccab58e38702ef5f4
rinaldy95 <rinaldymarasabessy@gmail.com>
2016-02-29 23:33:35 +0800

    update controler role dan regioanal

---
+++ aplikasi-distribusi-web/src/main/java/com/makananbekuenak/kyurifood/exception/DataNotFoundException.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.makananbekuenak.kyurifood.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class DataNotFoundException extends RuntimeException {
+    
+    public DataNotFoundException(String message) {
+        super(message);
+    }
+    
+}
