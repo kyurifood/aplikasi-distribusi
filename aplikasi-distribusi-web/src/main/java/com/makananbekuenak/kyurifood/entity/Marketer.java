bb06a35525f2dfcb17a8490044360d9570c23693
gilang <gilang.mhs13@ti.poligon.ac.id>
2016-03-04 16:23:18 +0700

    update marketer

--- aplikasi-distribusi-web/src/main/java/com/makananbekuenak/kyurifood/entity/Marketer.java
+++ aplikasi-distribusi-web/src/main/java/com/makananbekuenak/kyurifood/entity/Marketer.java
@@ -9,7 +9,6 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Null;
 import org.hibernate.annotations.GenericGenerator;
 import org.hibernate.validator.constraints.NotEmpty;
 
@@ -37,8 +36,6 @@ public class Marketer {
     @Column(nullable = false)
     private String hp;
     
-    @NotNull @NotEmpty
-    @Column(nullable = false)
     private String pinbb;
     
     @NotNull @NotEmpty
